a8644884e87bd86dbeafbf03574c290006d7e928
ManashSwain <manash0802@gmail.com>
2024-06-07 23:44:08 +0530

    removed publicaly exposed mongo db url

--- db/connect.js
+++ db/connect.js
@@ -20,14 +20,3 @@ module.exports = connectDB ;
 
 
 
-// const mongoose = require("mongoose");
-
-// uri =
-//  "mongodb+srv://manash0802:Mlo7S4Syh9wcoOAZ@manash.fjanfjb.mongodb.net/?retryWrites=true&w=majority&appName=Manash"
-
-// const connectDB = async ()=>{
-//     console.log("Db connected")
-//   await mongoose.connect(uri);
-// }
-
-// module.exports = connectDB ;
